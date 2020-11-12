db0762b2822c44f95e32150847ae39f0406bb7c4
Arsenii <Fedorov-Ars@yandex.ru>
2020-11-12 22:10:34 +0300

    Spring Flux ver.1

--- src/main/java/ru/otus/spring/service/PresentationService.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring.service;
-
-import ru.otus.spring.entity.Book;
-
-import java.util.List;
-
-public interface PresentationService {
-    String convertBooksForShellPresentation(List<Book> books);
-}
