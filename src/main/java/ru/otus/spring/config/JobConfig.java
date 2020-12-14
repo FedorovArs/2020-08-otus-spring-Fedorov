017cf5c14329f687405105556172a03d834cc39e
Arsenii Fedorov <Fedorov-Ars@yandex.ru>
2020-12-15 01:28:26 +0300

    Add listeners + Types

--- src/main/java/ru/otus/spring/config/JobConfig.java
+++ src/main/java/ru/otus/spring/config/JobConfig.java
@@ -1,17 +1,16 @@
 package ru.otus.spring.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.SessionFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.data.MongoItemWriter;
 import org.springframework.batch.item.database.HibernateCursorItemReader;
 import org.springframework.batch.item.database.builder.HibernateCursorItemReaderBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -19,47 +18,109 @@ import ru.otus.spring.entity.nosql.MongoBook;
 import ru.otus.spring.entity.sql.Book;
 import ru.otus.spring.service.ConvertEntityServiceImpl;
 
+import java.util.List;
 
-@SuppressWarnings("all")
+
 @Configuration
+@Slf4j
+@RequiredArgsConstructor
 public class JobConfig {
     private static final int CHUNK_SIZE = 5;
-    private final Logger logger = LoggerFactory.getLogger("Batch");
-    public static final String IMPORT_USER_JOB_NAME = "importUserJob";
 
-    @Autowired
-    private JobBuilderFactory jobBuilderFactory;
-
-    @Autowired
-    private StepBuilderFactory stepBuilderFactory;
-
-    @Autowired
-    private MongoTemplate mongoTemplate;
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job postgresToMongoDbmigrationJob(Step migrateRowsFromPostgresStep) {
+    public Job postgresToMongoDbMigrationJob(Step migrateRowsFromPostgresStep) {
         return jobBuilderFactory.get("postgresToMongoDbMigrationJob")
                 .start(migrateRowsFromPostgresStep)
                 .build();
     }
 
+//    @Bean
+//    public Step migrateRowsFromPostgresStep(HibernateCursorItemReader<Book> sqlBookReader, ItemProcessor<Book, MongoBook> convertBookToMongoBookProcessor,
+//                                            MongoItemWriter<MongoBook> noSqlBookWriter) {
+//        return stepBuilderFactory.get("migrateRowsFromPostgresStep")
+//                .<Book, Book>chunk(CHUNK_SIZE)
+//                .reader(sqlBookReader)
+//                .processor(convertBookToMongoBookProcessor)  // Вот этот метод имеет неверную сигнатуру. Почему-то требуется обязательное наследование от класса Book. Как выполнить приобразование одного объекта в другой я так и не понял =(
+//                .writer(noSqlBookWriter)
+//                .build();
+//    }
+
+    //    Закоментированный метод выше - это моя попытка сделать ItemProcessor типизированным, но тогда проект не собирается
     @Bean
-    public Step migrateRowsFromPostgresStep(HibernateCursorItemReader sqlBookReader, ItemProcessor convertBookToMongoBookProcessor, MongoItemWriter<MongoBook> noSqlBookWriter) {
+    public Step migrateRowsFromPostgresStep(HibernateCursorItemReader<Book> sqlBookReader, ItemProcessor convertBookToMongoBookProcessor,
+                                            MongoItemWriter<MongoBook> noSqlBookWriter) {
         return stepBuilderFactory.get("migrateRowsFromPostgresStep")
-                .chunk(CHUNK_SIZE)
+                .<Book, Book>chunk(CHUNK_SIZE)
                 .reader(sqlBookReader)
-                .processor(convertBookToMongoBookProcessor)
+                .processor(convertBookToMongoBookProcessor)// виной всему, вот эта строчка
                 .writer(noSqlBookWriter)
+                .listener(new ItemReadListener<Book>() {
+                    public void beforeRead() {
+                        log.info("Read start");
+                    }
+
+                    public void afterRead(Book o) {
+                        log.info("Read end");
+                    }
+
+                    public void onReadError(Exception e) {
+                        log.info("Read error " + e);
+                    }
+                })
+                .listener(new ItemWriteListener<MongoBook>() {
+                    public void beforeWrite(List<? extends MongoBook> list) {
+                        log.info("Write start");
+                    }
+
+                    public void afterWrite(List<? extends MongoBook> list) {
+                        log.info("Write end");
+                    }
+
+                    public void onWriteError(Exception e, List<? extends MongoBook> list) {
+                        log.info("Write error " + e);
+                    }
+                })
+                .listener(new ItemProcessListener<Book, MongoBook>() {
+                    public void beforeProcess(Book o) {
+                        log.info("Start process");
+                    }
+
+                    public void afterProcess(Book o, MongoBook o2) {
+                        log.info("End process");
+                    }
+
+                    @Override
+                    public void onProcessError(Book book, Exception e) {
+                        log.info("Process error " + e);
+                    }
+
+                })
+                .listener(new ChunkListener() {
+                    public void beforeChunk(ChunkContext chunkContext) {
+                        log.info("Start chunk");
+                    }
+
+                    public void afterChunk(ChunkContext chunkContext) {
+                        log.info("End chunk");
+                    }
+
+                    public void afterChunkError(ChunkContext chunkContext) {
+                        log.info("Chunk error " + chunkContext.toString());
+                    }
+                })
                 .build();
     }
 
     @Bean
-    public ItemProcessor convertBookToMongoBookProcessor(ConvertEntityServiceImpl convertEntityService) {
-        return (ItemProcessor<Book, MongoBook>) convertEntityService::convertSqlBookEntityToMongoBook;
+    public ItemProcessor<Book, MongoBook> convertBookToMongoBookProcessor(ConvertEntityServiceImpl convertEntityService) {
+        return convertEntityService::convertSqlBookEntityToMongoBook;
     }
 
     @Bean
-    public HibernateCursorItemReader sqlBookReader(SessionFactory sessionFactory) {
+    public HibernateCursorItemReader<Book> sqlBookReader(SessionFactory sessionFactory) {
         return new HibernateCursorItemReaderBuilder<Book>()
                 .name("sqlBookReader")
                 .sessionFactory(sessionFactory)
@@ -68,135 +129,10 @@ public class JobConfig {
     }
 
     @Bean
-    public MongoItemWriter<MongoBook> noSqlBookWriter() {
-        MongoItemWriter<MongoBook> writer = new MongoItemWriter<MongoBook>();
+    public MongoItemWriter<MongoBook> noSqlBookWriter(MongoTemplate mongoTemplate) {
+        MongoItemWriter<MongoBook> writer = new MongoItemWriter<>();
         writer.setTemplate(mongoTemplate);
         writer.setCollection("mongoBooks");
         return writer;
     }
-
-//    @StepScope
-//    @Bean
-//    public FlatFileItemReader<Person> reader(@Value("#{jobParameters['" + INPUT_FILE_NAME + "']}") String inputFileName) {
-//        return new FlatFileItemReaderBuilder<Person>()
-//                .name("personItemReader")
-//                .resource(new FileSystemResource(inputFileName))
-//
-//                // Работа через lineMapper
-//                .lineMapper((s, i) -> {
-//                    String[] fieldsValues = s.split(",");
-//                    return new Person(fieldsValues[0], Integer.parseInt(fieldsValues[1]));
-//                })
-///*
-//
-//                // Работа через fieldSetMapper
-//                .delimited()
-//                .names("name", "age")
-//                .fieldSetMapper(new BeanWrapperFieldSetMapper<>() {{
-//                    setTargetType(Person.class);
-//                }})
-//*/
-//
-//
-//                .build();
-//    }
-//
-//    @StepScope
-//    @Bean
-//    public ItemProcessor processor(HappyBirthdayService happyBirthdayService) {
-//        return (ItemProcessor<Person, Person>) happyBirthdayService::doHappyBirthday;
-//    }
-//
-//    @StepScope
-//    @Bean
-//    public FlatFileItemWriter writer(@Value("#{jobParameters['" + OUTPUT_FILE_NAME + "']}") String outputFileName) {
-//        return new FlatFileItemWriterBuilder<>()
-//                .name("personItemWriter")
-//                .resource(new FileSystemResource(outputFileName))
-//                .lineAggregator(new DelimitedLineAggregator<>())
-//                .build();
-//    }
-//
-//    @Bean
-//    public Job importUserJob(Step step1) {
-//        return jobBuilderFactory.get(IMPORT_USER_JOB_NAME)
-//                .incrementer(new RunIdIncrementer())
-//                .flow(step1)
-//                .end()
-//                .listener(new JobExecutionListener() {
-//                    @Override
-//                    public void beforeJob(JobExecution jobExecution) {
-//                        logger.info("Начало job");
-//                    }
-//
-//                    @Override
-//                    public void afterJob(JobExecution jobExecution) {
-//                        logger.info("Конец job");
-//                    }
-//                })
-//                .build();
-//    }
-//
-//    @Bean
-//    public Step step1(FlatFileItemWriter writer, ItemReader reader, ItemProcessor itemProcessor) {
-//        return stepBuilderFactory.get("step1")
-//                .chunk(CHUNK_SIZE)
-//                .reader(reader)
-//                .processor(itemProcessor)
-//                .writer(writer)
-//                .listener(new ItemReadListener() {
-//                    public void beforeRead() {
-//                        logger.info("Начало чтения");
-//                    }
-//
-//                    public void afterRead(Object o) {
-//                        logger.info("Конец чтения");
-//                    }
-//
-//                    public void onReadError(Exception e) {
-//                        logger.info("Ошибка чтения");
-//                    }
-//                })
-//                .listener(new ItemWriteListener() {
-//                    public void beforeWrite(List list) {
-//                        logger.info("Начало записи");
-//                    }
-//
-//                    public void afterWrite(List list) {
-//                        logger.info("Конец записи");
-//                    }
-//
-//                    public void onWriteError(Exception e, List list) {
-//                        logger.info("Ошибка записи");
-//                    }
-//                })
-//                .listener(new ItemProcessListener() {
-//                    public void beforeProcess(Object o) {
-//                        logger.info("Начало обработки");
-//                    }
-//
-//                    public void afterProcess(Object o, Object o2) {
-//                        logger.info("Конец обработки");
-//                    }
-//
-//                    public void onProcessError(Object o, Exception e) {
-//                        logger.info("Ошбка обработки");
-//                    }
-//                })
-//                .listener(new ChunkListener() {
-//                    public void beforeChunk(ChunkContext chunkContext) {
-//                        logger.info("Начало пачки");
-//                    }
-//
-//                    public void afterChunk(ChunkContext chunkContext) {
-//                        logger.info("Конец пачки");
-//                    }
-//
-//                    public void afterChunkError(ChunkContext chunkContext) {
-//                        logger.info("Ошибка пачки");
-//                    }
-//                })
-////                .taskExecutor(new SimpleAsyncTaskExecutor())
-//                .build();
-//    }
 }
--- src/main/java/ru/otus/spring/shell/BatchCommands.java
+++ src/main/java/ru/otus/spring/shell/BatchCommands.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.JobOperator;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
@@ -14,6 +16,8 @@ public class BatchCommands {
 
     private final Job postgresToMongoDbMigrationJob;
     private final JobLauncher jobLauncher;
+    private final JobOperator jobOperator;
+    private final JobExplorer jobExplorer;
 
     //http://localhost:8080/h2-console/
 
@@ -24,18 +28,16 @@ public class BatchCommands {
         System.out.println(execution);
     }
 
-//    @ShellMethod(value = "startMigrationJobWithJobOperator", key = "sm-jo")
-//    public void startMigrationJobWithJobOperator() throws Exception {
-//        Long executionId = jobOperator.start(IMPORT_USER_JOB_NAME,
-//                INPUT_FILE_NAME + "=" + appProps.getInputFile() + "\n" +
-//                        OUTPUT_FILE_NAME + "=" + appProps.getOutputFile()
-//        );
-//        System.out.println(jobOperator.getSummary(executionId));
-//    }
-//
-//    @ShellMethod(value = "showInfo", key = "i")
-//    public void showInfo() {
-//        System.out.println(jobExplorer.getJobNames());
-//        System.out.println(jobExplorer.getLastJobInstance(IMPORT_USER_JOB_NAME));
-//    }
+    @ShellMethod(value = "startMigrationJobWithJobOperator", key = "sm-jo")
+    public void startMigrationJobWithJobOperator() throws Exception {
+        Long executionId = jobOperator.start("postgresToMongoDbMigrationJob", "");
+        System.out.println(jobOperator.getSummary(executionId));
+    }
+
+    @ShellMethod(value = "showInfo", key = "i")
+    public void showInfo() {
+        System.out.println(jobExplorer.getJobNames());
+        System.out.println(jobExplorer.getLastJobInstance("postgresToMongoDbMigrationJob"));
+    }
 }
+
